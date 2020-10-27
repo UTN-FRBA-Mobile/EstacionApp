823c9ad36e21ea3ead3b4765285b9ea01e5b92e8
Ezequiel-Ogando <oguisan_90@hotmail.com>
2020-10-27 14:34:05 -0300

    camera reporting

---
+++ app/src/main/java/com/example/estacionapp/UploadApis.kt
@@ -0,0 +1,16 @@
+package com.example.estacionapp
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface UploadApis {
+    @Multipart
+    @POST("upload")
+    fun uploadImage(@Part part : MultipartBody.Part, @Part("user_id") requestBody : RequestBody) : Call<ResponseBody>
+
+}
